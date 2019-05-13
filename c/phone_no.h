9b3e3f21b9536f727dc55d48baf4dcc21adfd143
code <code@yeejay.com>
2019-05-13 16:52:46 +0800

    cleanup

--- c/phone_no.h
+++ c/phone_no.h
@@ -10,7 +10,6 @@ int cc_match(const char *no);
 int cc_match_ld(const char *no, int *ld, char **iso);
 // optional, if not called, built-in list will be used
 void cc_add(const char *cc, const char *iso);
-int parse_phone_no(char *phone, char *default_cc);
 
 #ifdef __cplusplus
 }
