a1ddc53ddbe12a414012b31b61dc8ac605600d7f
Steven I <you@example.com>
2020-01-02 01:17:47 +0800

    c# impl.

---
+++ cs/PhoneNr.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ippv
+{
+    class Node
+    {
+        public int type_;
+        public string iso_;
+        public Node[] children_;
+    }
+    class PhoneNr
+    {
+        public static Node[] tree_;
+
+        public static void init()
+        {
+            tree_ = new Node[10];
+        }
+
+        public static void add(string cc, string iso)
+        {
+            var tree = tree_;
+            for (int i = 0; i < cc.Length; ++i)
+            {
+                var digit = cc[i] - '0';
+                var node = tree[digit];
+                if (node == null)
+                {
+                    node = new Node();
+                    tree[digit] = node;
+                }
+                if (i == cc.Length - 1)
+                {
+                    node.type_ = 1;
+                    node.iso_ = iso;
+                    break;
+                }
+                if (node.children_ != null)
+                {
+                    tree = node.children_;
+                    continue;
+                }
+                node.children_ = new Node[10];
+                tree = node.children_;
+            }
+        }
+
+        public static int match(string no /*phone no*/, ref int ld /*leading digits*/, ref int ac /*area code*/, ref string iso /*country iso code*/)
+        {
+            int len = 0;
+            var tree = tree_;
+            for (var i = 0; i < no.Length && tree != null; ++i)
+            {
+                var digit = no[i] - '0';
+                var node = tree[digit];
+                if (node == null)
+                    break;
+
+                if (node.type_ == 1 || node.children_ == null)
+                {
+                    if (len == 0) len = i + 1;
+                    else if (ld == 0) ld = i + 1;
+                    else ac = i + 1;
+                    iso = node.iso_;
+                }
+
+                tree = node.children_;
+            }
+            return len;
+        }
+
+    }
+}
---
+++ cs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ippv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneNr.init();
+            PhoneNr.add("80", "C80");
+            PhoneNr.add("801", "C801");
+            PhoneNr.add("8011", "C8011");
+            PhoneNr.add("82", "C82");
+            PhoneNr.add("821", "C821");
+            PhoneNr.add("8211", "C8211");
+
+            int cc, ld, ac;
+            string iso;
+            cc = 0; ld = 0; ac = 0; iso = null;
+            cc = PhoneNr.match("8095555", ref ld, ref ac, ref iso);
+            cc = 0; ld = 0; ac = 0; iso = null;
+            cc = PhoneNr.match("80195555", ref ld, ref ac, ref iso);
+            cc = 0; ld = 0; ac = 0; iso = null;
+            cc = PhoneNr.match("801195555", ref ld, ref ac, ref iso);
+            cc = 0; ld = 0; ac = 0; iso = null;
+            cc = PhoneNr.match("821195555", ref ld, ref ac, ref iso);
+        }
+    }
+}
