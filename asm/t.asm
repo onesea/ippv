19202fd394472e5e1a8101fa4583a3c9781b57e8
root <root@debian>
2020-01-13 14:55:15 +0800

    up

--- asm/t.asm
+++ asm/t.asm
@@ -3,24 +3,16 @@ section .text
     global main 
     extern printf, scanf
 
-stack_size equ 40
+stack_size equ 48
 main:
     sub     rsp, stack_size
 
+    lea     rdi, [rel fmt_str]
     mov     rsi, rsp 
-    lea     rdi, [rel scanf_str]
     xor     eax, eax 
     call    scanf
-    cmp     rax, 1
-    jne     .exit
 
-    lea     rdi, [rel printf_str]
-    mov     rsi, [rsp]
-    xor     eax, eax 
-    call    printf
-.exit:
     add     rsp, stack_size
     ret
 
-scanf_str:  db "%ld",0
-printf_str: db "%ld",10,0
+fmt_str:  db "%ld",0
