a7a946f4d7d3946c8d976798012330771e2f36da
Steven I <you@example.com>
2020-03-01 23:25:26 +0800

    dart

--- dart/bin/main.dart
+++ dart/bin/main.dart
@@ -1,14 +1,19 @@
 import 'package:dart/phone.dart' as phone;
 
-void print_phone(nr) => print('${nr.cc} ${nr.ld} ${nr.ac} ${nr.iso}');
+void print_phone(nr,res) => print('${nr} -> ${res.cc} ${res.ld} ${res.ac} ${res.iso}');
+
+void parse_phone(nr) {
+	var res = phone.match(nr);
+	print_phone(nr,res);
+}
 
 void main(List<String> arguments) {
-  phone.add('1', 'us');
-  phone.add('120', 'ca');
-  phone.add('120300', 'mv');
+	phone.add('1', 'us');
+	phone.add('120', 'ca');
+	phone.add('120300', 'mv');
 
-  print_phone(phone.match('1211'));
-  print_phone(phone.match('1201'));
-  print_phone(phone.match('1203001'));
-  print_phone(phone.match('1203011'));
+	parse_phone('1211');
+	parse_phone('1201');
+	parse_phone('1203001');
+	parse_phone('1203011');
 }
