e38083c473f46308de2ad8d38318948e29456e28
Steven I <you@example.com>
2020-04-05 15:40:21 +0800

    cs

--- cs/Program.cs
+++ cs/Program.cs
@@ -11,28 +11,23 @@ namespace ippv
         static void Main(string[] args)
         {
             PhoneNr.init();
-            PhoneNr.add("80", "C80");
-            PhoneNr.add("801", "C801");
-            PhoneNr.add("8011", "C8011");
-            PhoneNr.add("82", "C82");
-            PhoneNr.add("821", "C821");
-            PhoneNr.add("8211", "C8211");
+            var cc_arr = new string[,] { { "80", "C80" },{ "801", "C801" },{ "8011", "C8011" },{ "82", "C82" },{ "821", "C821" },{ "8211", "C8211" } };
+            for (int i = 0; i < cc_arr.Length/2; i++)
+            {
+                PhoneNr.add(cc_arr[i, 0], cc_arr[i, 1]);
+            }
 
-            int cc, ld, ac;
-            string iso;
-            string phone;
-            phone = "8095555"; cc = 0; ld = 0; ac = 0; iso = null;
-            cc = PhoneNr.match(phone, ref ld, ref ac, ref iso);
-            Console.WriteLine(phone + " -> " + cc + "," + ld + "," + ac + "," + iso);
-            phone="80195555"; cc = 0; ld = 0; ac = 0; iso = null;
-            cc = PhoneNr.match(phone, ref ld, ref ac, ref iso);
-            Console.WriteLine(phone + " -> " + cc + "," + ld + "," + ac + "," + iso);
-            phone = "801195555"; cc = 0; ld = 0; ac = 0; iso = null;
-            cc = PhoneNr.match(phone, ref ld, ref ac, ref iso);
-            Console.WriteLine(phone + " -> " + cc + "," + ld + "," + ac + "," + iso);
-            phone = "821195555"; cc = 0; ld = 0; ac = 0; iso = null;
-            cc = PhoneNr.match(phone, ref ld, ref ac, ref iso);
-            Console.WriteLine(phone + " -> " + cc + "," + ld + "," + ac + "," + iso);
+            cc_arr = null;
+
+            var phone_arr = new string[] { "8095555", "80195555", "801195555" , "821195555" };
+            for (int i = 0; i < phone_arr.Length; i++)
+            {
+                int cc = 0, ld = 0, ac = 0;
+                string iso = null;
+                string phone = phone_arr[i];
+                cc = PhoneNr.match(phone, ref ld, ref ac, ref iso);
+                Console.WriteLine(phone + " -> " + cc + "," + ld + "," + ac + "," + iso);
+            }
         }
     }
 }
