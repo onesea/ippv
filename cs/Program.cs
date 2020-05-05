b0fcedd4e458f9bb58644964fa3f4d0cec8214ce
Steven I <you@example.com>
2020-05-05 10:41:12 +0800

    c# linq

--- cs/Program.cs
+++ cs/Program.cs
@@ -8,23 +8,26 @@ namespace ippv
 {
     class Program
     {
-        static void Main(string[] args)
+        static void init()
         {
             PhoneNr.init();
-            var cc_arr = new string[,] { { "80", "C80" },{ "801", "C801" },{ "8011", "C8011" },{ "82", "C82" },{ "821", "C821" },{ "8211", "C8211" } };
-            for (int i = 0; i < cc_arr.Length/2; i++)
+            var cc_arr = new string[,] { { "80", "C80" }, { "801", "C801" }, { "8011", "C8011" }, { "82", "C82" }, { "821", "C821" }, { "8211", "C8211" } };
+            for (int i = 0; i < cc_arr.Length / 2; i++)
             {
                 PhoneNr.add(cc_arr[i, 0], cc_arr[i, 1]);
             }
-
-            cc_arr = null;
+        }
+        static void Main(string[] args)
+        {
+            init();
 
             var phone_arr = new string[] { "8095555", "80195555", "801195555" , "821195555" };
-            for (int i = 0; i < phone_arr.Length; i++)
+            var query = from item in phone_arr where ! string.IsNullOrEmpty(item) select item;
+
+            foreach(var phone in query)
             {
                 int cc = 0, ld = 0, ac = 0;
                 string iso = null;
-                string phone = phone_arr[i];
                 cc = PhoneNr.match(phone, ref ld, ref ac, ref iso);
                 Console.WriteLine(phone + " -> " + cc + "," + ld + "," + ac + "," + iso);
             }
