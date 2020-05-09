93681b4c25ac1a4b693b279b3a0efaefb20098a9
root <root@t>
2020-05-09 09:54:35 +0800

    up

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -1,4 +1,4 @@
-val gradle_version = "6.4"
+val gradle_version = "6.5-milesone-1"
 
 buildscript {
 	repositories {
--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val gradle_version = "6.4"
+val gradle_version = "6.5-milestone-1"
 
 buildscript {
 	extra.apply {
